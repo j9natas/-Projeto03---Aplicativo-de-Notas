fddbb3c9cdd254f636b8f13cb796960ea8787226
j9natas <147943646+j9natas@users.noreply.github.com>
2024-06-13 19:29:42 -0300

    Add files via upload

---
+++ Aplicativo de Notas03/ArquivoUtil.java
@@ -0,0 +1,18 @@
+package notas02;
+
+public class ArquivoUtil {
+
+	
+	 public static void salvarTexto(String texto, String caminhoArquivo) throws Exception {
+
+	        java.io.FileWriter writer = new java.io.FileWriter(caminhoArquivo);
+
+	        writer.write(texto);
+
+	        writer.close();
+
+	    }
+	
+
+	
+}
